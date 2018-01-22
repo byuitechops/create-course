10b2795e7e01ebde8596bb1bda769def506ac326
Zachary Williams <wil11063@byui.edu>
2018-01-22 16:33:13 -0700

    New Log

--- createCourse.js
+++ createCourse.js
@@ -3,7 +3,7 @@
 
 /* Put dependencies here */
 const request = require('request'),
-   auth = require('../../auth.json');
+    auth = require('../../auth.json');
 
 
 /**************************************
@@ -11,53 +11,52 @@ const request = require('request'),
  * saves canvasOU to the course object
  **************************************/
 module.exports = (course, stepCallback) => {
-   course.addModuleReport("createCourse");
 
-   if (course.settings.canvasOU) {
-      course.newInfo('canvasOU', course.settings.canvasOU);
-      course.success('CreateCourse', `Determined to use existing course with the CanvasOU of ${course.settings.canvasOU}`);
-      stepCallback(null, course);
-      return;
-   }
+    if (course.settings.canvasOU) {
+        course.newInfo('canvasOU', course.settings.canvasOU);
+        course.message(`Canvas OU was already provided. A new course was not created. Course ID used: ${course.settings.canvasOU}`);
+        stepCallback(null, course);
+        return;
+    }
 
 
-   var courseName = '',
-      courseCode = '';
+    var courseName = '',
+        courseCode = '';
 
-   /* Get Canvas course name from the fileName */
-   if ((/\d{3}\w?/i).test(course.info.fileName)) {
-      courseCode = course.info.fileName.match(/\d{3}\w?/i)[0];
-      courseName = course.info.fileName.split(/\d{3}\w?/i)[0].trim();
-   } else {
-      courseName = course.info.fileName.split('.zip')[0];
-      courseCode = course.info.fileName.split('.zip')[0];
-      // courseCode = '101';
-   }
+    /* Get Canvas course name from the fileName */
+    if ((/\d{3}\w?/i).test(course.info.fileName)) {
+        courseCode = course.info.fileName.match(/\d{3}\w?/i)[0];
+        courseName = course.info.fileName.split(/\d{3}\w?/i)[0].trim();
+    } else {
+        courseName = course.info.fileName.split('.zip')[0];
+        courseCode = course.info.fileName.split('.zip')[0];
+        // courseCode = '101';
+    }
 
-   request.post({
-      url: "https://byui.instructure.com/api/v1/accounts/19/courses",
-      form: {
-         'course[name]': courseName,
-         'course[course_code]': courseCode,
-         'course[license]': "public_domain",
-         'course[is_public_to_auth_users]': "true"
-      }
-   }, function (err, response, body) {
-      if (err) {
-         course.throwFatalErr("createCourse", err);
-         stepCallback(err, course);
-         return;
-      } else {
-         //console.log(chalk.green(courseName + " Successfully created"));
-         //console.log('Course Number: ', body.id);
-         body = JSON.parse(body);
+    request.post({
+        url: "https://byui.instructure.com/api/v1/accounts/19/courses",
+        form: {
+            'course[name]': courseName,
+            'course[course_code]': courseCode,
+            'course[license]': "public_domain",
+            'course[is_public_to_auth_users]': "true"
+        }
+    }, function (err, response, body) {
+        if (err) {
+            course.fatalError(err);
+            stepCallback(err, course);
+            return;
+        } else {
+            //console.log(chalk.green(courseName + " Successfully created"));
+            //console.log('Course Number: ', body.id);
+            body = JSON.parse(body);
 
-         //course.info.canvasOU = body.id;
-         course.newInfo('canvasOU', body.id);
+            //course.info.canvasOU = body.id;
+            course.newInfo('canvasOU', body.id);
 
-         //course.report.moduleLogs['importCourse'].changes.push('Course successfully created in Canvas');
-         course.success("createCourse", "Course successfully created in Canvas");
-         stepCallback(null, course);
-      }
-   }).auth(null, null, true, auth.token);
+            //course.report.moduleLogs['importCourse'].changes.push('Course successfully created in Canvas');
+            course.message('Course successfully created in Canvas');
+            stepCallback(null, course);
+        }
+    }).auth(null, null, true, auth.token);
 };
