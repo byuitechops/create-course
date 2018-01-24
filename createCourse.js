5bc83ba5f61d10041be9f52ddb1b14e600a3fdc6
Danverde <gre12014@byui.edu>
2018-01-24 14:29:02 -0700

    fixing merge err

--- createCourse.js
+++ createCourse.js
@@ -11,7 +11,6 @@ const request = require('request'),
  * saves canvasOU to the course object
  **************************************/
 module.exports = (course, stepCallback) => {
-<<<<<<< HEAD
 
     if (course.settings.canvasOU) {
         course.newInfo('canvasOU', course.settings.canvasOU);
@@ -61,53 +60,3 @@ module.exports = (course, stepCallback) => {
         }
     }).auth(null, null, true, auth.token);
 };
-=======
-   if (course.settings.canvasOU) {
-      course.newInfo('canvasOU', course.settings.canvasOU);
-      course.log('Create Canvas Course', {'Canvas Course OU': course.settings.canvasOU}); // ,`Determined to use existing course with the CanvasOU of ${course.settings.canvasOU}`);
-      stepCallback(null, course);
-      return;
-   }
-
-
-   var courseName = '',
-      courseCode = '';
-
-   /* Get Canvas course name from the fileName */
-   if ((/\d{3}\w?/i).test(course.info.fileName)) {
-      courseCode = course.info.fileName.match(/\d{3}\w?/i)[0];
-      courseName = course.info.fileName.split(/\d{3}\w?/i)[0].trim();
-   } else {
-      courseName = course.info.fileName.split('.zip')[0];
-      courseCode = course.info.fileName.split('.zip')[0];
-      // courseCode = '101';
-   }
-
-   request.post({
-      url: "https://byui.instructure.com/api/v1/accounts/19/courses",
-      form: {
-         'course[name]': courseName,
-         'course[course_code]': courseCode,
-         'course[license]': "public_domain",
-         'course[is_public_to_auth_users]': "true"
-      }
-   }, function (err, response, body) {
-      if (err) {
-         course.FatalError(err);
-         stepCallback(err, course);
-         return;
-      } else {
-         //console.log(chalk.green(courseName + " Successfully created"));
-         //console.log('Course Number: ', body.id);
-         body = JSON.parse(body);
-
-         //course.info.canvasOU = body.id;
-         course.newInfo('canvasOU', body.id);
-
-         //course.report.moduleLogs['importCourse'].changes.push('Course successfully created in Canvas');
-         course.log("Create Canvas Course", {'Canvas Course OU': body.id});
-         stepCallback(null, course);
-      }
-   }).auth(null, null, true, auth.token);
-};
->>>>>>> 024467a06d0c8264e891dff6ef925726e3f9f51c
